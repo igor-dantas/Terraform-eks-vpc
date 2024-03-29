9f1668e85ded4173134e5ddc59d3d5c04b109474
Igor Dantas <igor.dantas@eurekalabs.com.br>
2024-03-29 12:41:23 -0300

    feat: adding vpc

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+# resource "nome_do_recurso"  "nome_personalizado_do_recurso"
+resource "aws_vpc" "eks_vpc" {
+  cidr_block = "20.230.0.0/16"
+  #enable_dns_support = true é default, mas vamos deixar o mais verboso possível
+    enable_dns_support = true
+    enable_dns_hostnames = true
+    tags = {
+      Name = "eks-vpc"
+    }
+}
