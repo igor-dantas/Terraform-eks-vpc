44971bf553a7df20ff39a638ed172aba122e6883
Igor Dantas <igor.dantas@eurekalabs.com.br>
2024-03-29 12:51:56 -0300

    fix: using terraform fmt to format the configuration files

--- provider.tf
+++ provider.tf
@@ -4,13 +4,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "5.43.0"
     }
   }
 }
 
 provider "aws" {
-    region = "us-east-1"
-    profile = "igor-estudos"
+  region  = "us-east-1"
+  profile = "igor-estudos"
 }
